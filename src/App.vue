1f0062cf179dcea134890d3d15016f26038f2151
Toru Takeuchi <tofudesukedo@gmail.com>
2024-05-10 00:31:37 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="terms-container">
+    <h1 class="terms-title">特定商取引法に基づく表記</h1>
+    <table class="terms-table">
+      <tr>
+        <td>商号：</td>
+        <td>竹内徹</td>
+      </tr>
+      <tr>
+        <td>代表者名：</td>
+        <td>竹内徹</td>
+      </tr>
+      <tr>
+        <td>所在地：</td>
+        <td>〒426-0071 静岡県藤枝市志太3-16-16</td>
+      </tr>
+      <tr>
+        <td>電話番号：</td>
+        <td>08077458256</td>
+      </tr>
+      <tr>
+        <td>メールアドレス：</td>
+        <td>tofudesukedo@gmail.com</td>
+      </tr>
+      <tr>
+        <td>ホームページURL：</td>
+        <td>
+          <a
+            href="https://www.tiktok.com/@torubunol?is_from_webapp=1&sender_device=pc"
+            >TikTok</a
+          >
+        </td>
+      </tr>
+    </table>
+
+    <h2 class="terms-section-title">商品の価格</h2>
+    <p class="terms-info">9800円(表示価格はすべて税込み価格です。)</p>
+
+    <h2 class="terms-section-title">支払い方法</h2>
+    <p class="terms-info">クレジットカード</p>
+    <h2 class="terms-section-title">返金について</h2>
+    <p class="terms-info">商品の性質上、返品は受け付けておりません。</p>
+    <h2 class="terms-section-title">
+      特定商取引法に基づく表記に関するお問い合わせ先
+    </h2>
+    <table class="terms-table">
+      <tr>
+        <td>竹内徹</td>
+      </tr>
+      <tr>
+        <td>〒426-0071 静岡県藤枝市志太3-16-16</td>
+      </tr>
+      <tr>
+        <td>電話番号：080-7745-8256</td>
+      </tr>
+      <tr>
+        <td>メールアドレス：tofudesukedo@gmail.com</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "特定商取引法表記",
+  // コンポーネントのロジック
+};
+</script>
+
+<style scoped>
+.terms-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f8f8f8;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.terms-title {
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+
+.terms-section-title {
+  font-size: 24px;
+  margin-top: 30px;
+}
+
+.terms-info {
+  margin-bottom: 20px;
+}
+
+.terms-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+}
+
+.terms-table td {
+  padding: 8px;
+  border-bottom: 1px solid #ccc;
+}
+
+a {
+  color: blue;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
